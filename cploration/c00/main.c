b55b31e830f13ce8b731691da35170213581ee70
Brody Couture <couturbr@oregonstate.edu>
2024-09-27 17:29:48 -0700

    Csploration 0

---
+++ cploration/c00/main.c
@@ -0,0 +1,14 @@
+
+/**************************************** 
+ * C-ploration 0 for CS 271 
+ *   
+ * [NAME] Brody Couture 
+ * [TERM] FALL 2nd Year
+ *****************************************/
+#include <stdio.h>
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
