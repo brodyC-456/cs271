d576c162baf13d026c3766c1de52322752815f44
Brody Couture <couturbr@oregonstate.edu>
2024-11-05 14:47:20 -0800

    Fixed code

--- cploration/c06/parser.h
+++ cploration/c06/parser.h
@@ -15,6 +15,40 @@
 #define MAX_LINE_LENGTH  200
 #define MAX_LABEL_LENGTH MAX_LINE_LENGTH - 2
 
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+
+enum instr_type{
+	
+	Invalid = -1,
+	AType,
+	CType,
+	
+};
+
+typedef struct c_instruction{
+	opcode a:1;
+	opcode comp:6;
+	opcode dest:3;
+	opcode jump:3;
+}c_instruction;
+
+typedef struct a_instruction{
+	union type{
+		hack_addr address;
+		char * label;	
+	};
+	bool is_addr;
+} a_instruction;
+
+typedef struct instruction{
+	union instr{
+		a_instruction;
+		c_instruction;
+	};
+	enum instr_type instruction_type;
+} instruction;
+
 /** function prototypes **/
 char *strip(char *s);
 void parse(FILE * file);
@@ -23,39 +57,7 @@ bool is_label(const char *);
 bool is_CType(const char *);
 char *extract_label(const char *line, char* label);
 
-typedef int16_t hack_addr;
-typedef int16_t opcode;
 
-enum instr_type{
-	
-	Invalid = -1;
-	AType;
-	CType;
-	
-}
-
-typedef struct c_instruction{
-	opcode a:1;
-	opcode comp:6;
-	opcode dest:3;
-	opcode jump:3;
-}
-
-typedef struct a_instruction{
-	union type{
-		hack_addr address;
-		char *label;
-	};
-	bool is_addr;
-}
-
-typedef struct instruction{
-	union instr{
-		a_instruction;
-		c_instruction;
-	}
-	instr_type instruction_type;
-}
 
 
 
